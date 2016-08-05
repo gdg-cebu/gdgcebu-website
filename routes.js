b7c94ca3b2e9187e92efdaea962ce42f2b3139b2
Arnelle Balane <arnellebalane@gmail.com>
2016-08-05 17:25:54 +0800

    fetch events data from firebase and render them

--- routes.js
+++ routes.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var firebase = require('firebase');
+var config  = require('./config');
+
 var router = new express.Router();
+
+
+firebase.initializeApp({
+    databaseURL: config.get('FIREBASE_DATABASE_URL')
+});
+var database = firebase.database();
 
 
 router.use(function(request, response, next) {
@@ -24,6 +33,22 @@ router.get('/events', function(request, response) {
 router.get('/team', function(request, response) {
     response.render('team.html');
 })
+
+
+router.get('/get-events', function(request, response) {
+    var eventsref = database.ref('events');
+    eventsref.on('value', function(data) {
+        var events = data.val().filter(function(event) {
+            return event !== null;
+        }).sort(function(a, b) {
+            var datea = new Date(a.date);
+            var dateb = new Date(b.date);
+            return datea.valueOf() - dateb.valueOf();
+        });
+        eventsref.off('value');
+        response.json(events);
+    });
+});
 
 
 module.exports = router;
---
+++ static/javascripts/events.js
@@ -0,0 +1,40 @@
+function fetchEvents() {
+    return fetch('/get-events').then(function(response) {
+        return response.json();
+    });
+}
+
+
+function renderEvent(container, template, data) {
+    var event = $(template);
+    event.find('.event__thumbnail')
+        .attr('src', data.thumbnail)
+        .attr('alt', data.name);
+    event.find('.event__title').text(data.name);
+    event.find('.event__info--when').text(data.date);
+    event.find('.event__info--where').text(data.venue);
+    event.find('p').text(data.description);
+
+    if (!data.venue) {
+        event.find('.event__info--where').addClass('hidden');
+    }
+
+    container.append(event);
+    return event;
+}
+
+
+
+var eventsContainer = $('.events');
+var eventTemplate = $('template#event').html();
+
+fetchEvents().then(function(events) {
+    events.forEach(function(event) {
+        if (!event.thumbnail) {
+            event.thumbnail = 'gdg-logo-gray.png';
+        }
+        event.thumbnail = '/static/images/' + event.thumbnail;
+
+        renderEvent(eventsContainer, eventTemplate, event);
+    });
+});
