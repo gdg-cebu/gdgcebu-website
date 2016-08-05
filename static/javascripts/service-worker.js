ac954f00c9e7aa8cd49cf2fe5730d1bc233c6091
Arnelle Balane <arnellebalane@gmail.com>
2016-08-05 21:57:09 +0800

    cache assets offline with service worker

---
+++ static/javascripts/service-worker.js
@@ -0,0 +1,76 @@
+var cacheName = 'cache-v1';
+var urlsToCache = [
+    '/',
+    '/events',
+    '/team',
+    '/static/stylesheets/base.css',
+    '/static/stylesheets/utilities.css',
+    '/static/stylesheets/about.css',
+    '/static/stylesheets/events.css',
+    '/static/stylesheets/team.css',
+    '/static/javascripts/base.js',
+    '/static/javascripts/events.js',
+    '/static/vendor/jquery/dist/jquery.min.js',
+    '/static/manifest.json',
+    '/static/images/backgrounds/1.jpg',
+    '/static/images/events/io2016.png',
+    '/static/images/events/wtm.png',
+    '/static/images/team/arnelle.jpg',
+    '/static/images/team/ayrton.jpg',
+    '/static/images/team/julia.jpg',
+    '/static/images/team/mark.jpg',
+    '/static/images/team/rome.jpg',
+    '/static/images/calendar.png',
+    '/static/images/facebook.png',
+    '/static/images/gdg-cebu-logo.png',
+    '/static/images/gdg-logo-gray.png',
+    '/static/images/gdg-logo.png',
+    '/static/images/github.png',
+    '/static/images/location.png',
+    '/static/images/twitter.png',
+    '/static/images/website.png'
+];
+
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        }).then(function() {
+            self.skipWaiting();
+        }).catch(function(error) {
+            console.error('Error in service worker caching.', error);
+        })
+    );
+});
+
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(cacheKeys) {
+            return Promise.all(cacheKeys.map(function(cacheKey) {
+                if (cacheKey !== cacheName) {
+                    return caches.delete(cacheKey);
+                }
+            }));
+        }).catch(function(error) {
+            console.error('Error getting cache keys.', error);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.open(cacheName).then(function(cache) {
+            return cache.match(e.request).then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(e.request);
+            });
+        }).catch(function(error) {
+            console.error('Error opening cache.', error);
+        })
+    );
+});
